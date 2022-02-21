f7d74fb249d81fd8b29ae44e4d25e9ab1233c9f0
InacioMarcus <86126612+InacioMarcus@users.noreply.github.com>
2022-02-21 14:52:08 -0300

    Update calculos.js

--- calculos.js
+++ calculos.js
@@ -1,7 +1,15 @@
 function calcular(){
     let media= document.getElementById("final").value
+    if(media>=3.0){
     quantoPrecisa=-((media*0.6)-5)/0.4
     var valorfinal= Math.round(quantoPrecisa * 10)/10
     console.log(valorfinal)
-    document.getElementById("notadafinal").innerHTML=valorfinal+" ;" +" " + "Boa Sorte :)"
+    document.getElementById("notadafinal").innerHTML=valorfinal+" ;" +" " + "Boa Sorte :)"}
+    if(media<3.0){
+        document.getElementById("notadafinal").innerHTML='É serio mesmo fera ?'
+        
+    }
+    if(media>=7.0){
+        document.getElementById("notadafinal").innerHTML='Legal fera'
+    }
 }
